a593e4a2b51ad7a3b1e95aa9d2a2865b07a4c6f4
김형래 <khr157929@gmail.com>
2022-01-27 23:13:40 +0900

    백준 경로변경

--- baekJoon/단계별 문제/입출력과 사칙연산/10869.js
+++
@@ -1,9 +0,0 @@
-let input = require("fs").readFileSync("10869.txt").toString().split(" ");
-
-const A = parseInt(input[0]);
-const B = parseInt(input[1]);
-console.log(A + B);
-console.log(A - B);
-console.log(A * B);
-console.log(Math.floor(A / B));
-console.log(A % B);
